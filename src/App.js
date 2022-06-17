a7516f1de068e2e17e208966a5581143b4aac1df
Shaqnificent <shaqnificent@gmail.com>
2022-06-17 15:39:39 -0500

    intro to components, props and states

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import {useState} from 'react';
+
 
 const App = () =>  {
+  const Person = (props) => {
+    return (
+      <>
+        <h1>Name: {props.name} {props.lname}</h1>
+        <h2>Age: {props.age}</h2>
+      
+      </>
+    )
+  }
+
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <h1>Hello, React!</h1>
+        <Person name='Kevin' lname='Brown' age={20}/>
+        <button onClick={() => setCounter((prevCount)=> prevCount-1)}>-</button>
+        <h3>Score: {counter}</h3>
+        <button onClick={() => setCounter((prevCount)=> prevCount+1)}>+</button>
+        
     </div>
   );
 }
